93ac9fc094e619c25f56a864505c1ef6fbba19e2
gassysoil <zhangyanjohn@gmail.com>
2023-06-27 21:04:58 -0700

    commit

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+// This is the entrypoint of the CDK application
+// It will load the stack defined in lib/cdk-workshop-stack.ts
+
+import * as cdk from "aws-cdk-lib";
+import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new CdkWorkshopStack(app, "CdkWorkshopStack");
